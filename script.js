7a0c8754e0261375f1cd71f9e66f2c365bf9ab05
Rahbar-Ali <optimusleader123@gmail.com>
2026-03-10 02:58:34 +0500

    Update script.js

--- script.js
+++ script.js
@@ -9,6 +9,18 @@ const account1 = {
   owner: 'Jonas Schmedtmann',
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
+  movementsDates: [
+    '2019-11-18T21:31:17.178Z',
+    '2019-12-23T07:42:02.383Z',
+    '2020-01-28T09:15:04.904Z',
+    '2020-04-01T10:17:24.185Z',
+    '2020-05-08T14:11:59.604Z',
+    '2020-05-27T17:01:17.194Z',
+    '2020-07-11T23:36:17.929Z',
+    '2020-07-12T10:51:36.790Z',
+  ],
+  currency: 'EUR',
+  locale: 'pt-PT', // de-DE
   pin: 1111,
 };
 
@@ -17,6 +29,18 @@ const account2 = {
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
+  movementsDates: [
+    '2019-11-01T13:15:33.035Z',
+    '2019-11-30T09:48:16.867Z',
+    '2019-12-25T06:04:23.907Z',
+    '2020-01-25T14:18:46.235Z',
+    '2020-02-05T16:33:06.386Z',
+    '2020-04-10T14:43:26.374Z',
+    '2020-06-25T18:49:59.371Z',
+    '2020-07-26T12:01:20.894Z',
+  ],
+  currency: 'USD',
+  locale: 'en-US',
 };
 
 const account3 = {
@@ -224,7 +248,104 @@ const groupedByActivity = Object.groupBy(accounts, account => {
   if (movementsCount >= 1) return 'Moderate';
   return 'inactive';
 });
-console.log(groupedByActivity);
+
+console.log(22 === 22.0);
+console.log(0.1 + 0.2);
+
+// console.log(groupedByActivity);
+
+/*
+
+Julia and Kate are still studying dogs. This time they are want to figure out if the dogs in their are eating too much or too little food.
+
+- Formula for calculating recommended food portion: recommendedFood = weight ** 0.75 * 28. (The result is in grams of food, and the weight needs to be in kg)
+- Eating too much means the dog's current food portion is larger than the recommended portion, and eating too little is the opposite.
+- Eating an okay amount means the dog's current food portion is within a range 10% above and below the recommended portion (see hint).
+
+YOUR TASKS:
+1. Loop over the array containing dog objects, and for each dog, calculate the recommended food portion (recFood) and add it to the object as a new property. Do NOT create a new array, simply loop over the array (We never did this before, so think about how you can do this without creating a new array).
+2. Find Sarah's dog and log to the console whether it's eating too much or too little. HINT: Some dogs have multiple users, so you first need to find Sarah in the owners array, and so this one is a bit tricky (on purpose) 🤓
+3. Create an array containing all owners of dogs who eat too much (ownersTooMuch) and an array with all owners of dogs who eat too little (ownersTooLittle).
+4. Log a string to the console for each array created in 3., like this: "Matilda and Alice and Bob's dogs eat too much!" and "Sarah and John and Michael's dogs eat too little!"
+5. Log to the console whether there is ANY dog eating EXACTLY the amount of food that is recommended (just true or false)
+6. Log to the console whether ALL of the dogs are eating an OKAY amount of food (just true or false)
+7. Create an array containing the dogs that are eating an OKAY amount of food (try to reuse the condition used in 6.)
+8. Group the dogs into the following 3 groups: 'exact', 'too-much' and 'too-little', based on whether they are eating too much, too little or the exact amount of food, based on the recommended food portion.
+9. Group the dogs by the number of owners they have
+10. Sort the dogs array by recommended food portion in an ascending order. Make sure to NOT mutate the original array!
+
+HINT 1: Use many different tools to solve these challenges, you can use the summary lecture to choose between them 😉
+HINT 2: Being within a range 10% above and below the recommended portion means: current > (recommended * 0.90) && current < (recommended * 1.10). Basically, the current portion should be between 90% and 110% of the recommended portion.
+
+TEST DATA:
+*/
+/*
+const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John', 'Leo'] },
+  { weight: 18, curFood: 244, owners: ['Joe'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] },
+];
+
+dogs.forEach(dog => {
+  dog.recFood = Math.floor(dog.weight ** 0.75 * 28);
+  console.log(dog);
+});
+
+const dogSarah = dogs.find(dog => dog.owners.includes('Sarah'));
+const message =
+  dogSarah.weight >= 16 ? 'its eating too much ' : 'Its eating to too less';
+console.log(message);
+
+const ownersToMuch = dogs
+  .filter(dog => dog.curFood > dog.recFood)
+  .flatMap(dog => dog.owners);
+
+const ownersToLess = dogs
+  .filter(dog => dog.curFood < dog.recFood)
+  .flatMap(dog => dog.owners)
+  .flat();
+console.log(ownersToMuch);
+console.log(ownersToLess);
+
+console.log(`${ownersToMuch.join(' ')}'s dogs are eating too much`);
+console.log(`${ownersToLess.join(' ')}'s dogs are eating too less`);
+
+const recommendedDog = dogs.some(dog => dog.curFood === dog.recFood);
+console.log(recommendedDog);
+
+const recommendedDog2 = dogs.some(dog => dog.curFood >= dog.recFood);
+console.log(recommendedDog2);
+
+const recommendedDog3 = dogs.find(dog => dog.curFood >= dog.recFood);
+console.log(recommendedDog3);
+// console.log(newDog);
+
+// const [...others] = dogs.filte r(dog => dog.curFood >= 250);
+
+// console.log(a, others);
+
+const friend = [
+  { name: 'Rahbar', friends: ['Ali', 'Hassan'] },
+  { name: 'Shamsher', friends: ['Waqar', 'Hassan', 'Hussain'] },
+];
+
+const aboutFrienda = friend
+  .filter(f => f.name.includes('Rahbar'))
+  .map(f => f.friends)
+  .join(' ');
+
+const aboutFriendb = friend
+  .filter(f => f.name.includes('Shamsherc'))
+  .map(f => f.friends)
+  .join(' ');
+
+const name = friend.map(n => n.name);
+const [a, b] = name;
+// console.log(`The Friend of ${a} are ${aboutFrienda}`);
+// console.log(`The Friend of ${b} are ${aboutFriendb}`);
+
 /*
 console.log(movements);
 const lastWithdrawal = movements.findLast(mov => mov < 0);
